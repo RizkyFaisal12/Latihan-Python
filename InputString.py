fda1fe62ea8acbf1085f81ff3ab294ac4bf96339
Rizky Faisal <rizkyfaisal.career@gmail.com>
2026-03-21 04:44:20 +0700

    Add files via upload

---
+++ InputString.py
@@ -0,0 +1,8 @@
+nama = str(input("Masukkan nama = "))
+jurusan = str(input("Masukkan jurusan = "))
+umur = int (input("Masukkan umur = "))
+tinggi = int (input("Masukkan tinggi = "))
+
+
+print (f"Halo saya {nama} dari jurusan {jurusan}, umur saya {umur} dan tinggi saya {tinggi}")
+print()
