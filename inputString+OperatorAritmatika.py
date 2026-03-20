5f827b2f8b845cc5bb36ea7f527d6524935bc43f
Rizky Faisal <rizkyfaisal.career@gmail.com>
2026-03-21 04:36:16 +0700

    Add files via upload

---
+++ inputString+OperatorAritmatika.py
@@ -0,0 +1,6 @@
+angka1 = int(input("Masukkan angka ke-1 = "))
+angka2 = int(input("Masukkan angka-2 = "))
+
+print (f"Hasil {angka1} + {angka2} = {angka1+angka2}")
+print (f"Hasil {angka1} x {angka2} = {angka1*angka2}")
+print (f"Hasil {angka1} % {angka2} = {angka1%angka2}")
